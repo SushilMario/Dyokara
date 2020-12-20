bedfde142458a729d296c7d0df3798fbba41b8e1
SushilMario <sushilmario@gmail.com>
2020-12-20 16:14:33 +0530

    Navbar toggling

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,8 +1,16 @@
 const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarLinkToggler = document.querySelector("nav-link dropdown-toggle");
 
 const initialContainer = document.querySelector(".initial-container");
 
 navbarToggler.addEventListener("click",
+    () =>
+    {
+        initialContainer.classList.toggle("move-down");
+    }
+)
+
+navbarLinkToggler.addEventListener("click",
     () =>
     {
         initialContainer.classList.toggle("move-down");
