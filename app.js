83b59965d6742772b2fc8142b949d8aebc751f7a
SushilMario <sushilmario@gmail.com>
2021-01-23 10:55:28 +0530

    Footer position fix

--- app.js
+++ app.js
@@ -225,28 +225,28 @@ app.use
 //     }
 // )
 
-const primaryTrack = 
-{
-    name: "primary",
-    currentOrderNumber: 1,
-    currentLineupNumber: 0,
-};
+// const primaryTrack = 
+// {
+//     name: "primary",
+//     currentOrderNumber: 1,
+//     currentLineupNumber: 0,
+// };
 
-Tracking.create(primaryTrack,
-    (err, track) =>
-    {
-        if(err)
-        {
-            console.log("Primary track could not be created");
-        }
-        else
-        {
-            console.log("Primary track created!");
-        }
-    }    
-)
+// Tracking.create(primaryTrack,
+//     (err, track) =>
+//     {
+//         if(err)
+//         {
+//             console.log("Primary track could not be created");
+//         }
+//         else
+//         {
+//             console.log("Primary track created!");
+//         }
+//     }    
+// )
 
-// User.findOne({username: "Dyokara Info"}, 
+// User.findOne({username: "Sushil Mario"}, 
 //     async(err, user) =>
 //     {
 //         if(err)
